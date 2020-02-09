2e052a9d84f7a83764a3ca3a253e5054bd3e70ff
quinnmicha <mquinn@email.neit.edu>
2020-02-09 16:01:03 -0500

    Modulated nav bar to be accessed from one spot, began converting register page to boostrap 4

--- InventoryManager/index.php
+++ InventoryManager/index.php
@@ -10,7 +10,7 @@ if(isPostRequest()){
     $login = login($user, $pass);
     if($login!=false){
         session_start();
-
+        $_SESSION['login'] = true;
         $_SESSION['username'] = $login[0]['username'];
         $_SESSION['usertype'] = $login[0]['group'];
         header("Location: ../InventoryManager/manager_home.php");
@@ -22,24 +22,18 @@ if(isPostRequest()){
   <title>Log In</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-  <link rel="stylesheet" href="Design/design.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-</head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="Design/design.css">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </head>
 <body>
     
+    <?php include __DIR__.'/model/navbar.php';?>
+    
 <div class="container">
-    <div class="row align-items-start">
-        <header>
-            <div class="block1">
 
-            </div>
-            <div class="block2">
-
-            </div>
-        </header>
-    </div>
     
     <div class="row align-items-center">
         <h2 style="text-align: center; margin-top: 10%; margin-bottom: 2%">Welcome to the Inventory Management System</h2>
