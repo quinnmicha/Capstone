2d09ebcb03cd8842f84ba49de61fb52501f3258a
dfernandez15 <dfernandez10@email.neit.edu>
2020-02-09 17:26:25 -0500

    Admin Home Page Updated

--- InventoryManager/index.php
+++ InventoryManager/index.php
@@ -16,52 +16,47 @@ if(isPostRequest()){
   <title>Log In</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
   <link rel="stylesheet" href="Design/design.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  
 </head>
 <body>
     
 <div class="container">
-    <div class="row align-items-start">
-        <header>
+    <div>
             <div class="block1">
 
             </div>
             <div class="block2">
 
             </div>
-        </header>
     </div>
     
-    <div class="row align-items-center">
-        <h2 style="text-align: center; margin-top: 10%; margin-bottom: 2%">Welcome to the Inventory Management System</h2>
-        <h3 style="text-align: center; margin-bottom: 8%">The Only Inventory You'll Ever Need !</h3>
-        <form class="form-horizontal" action="manager_home.php" method="post">
+    <div class="row justify-content-center">
+        <h2 style="margin-top: 10%; margin-bottom: 2%">Welcome to the Inventory Management System</h2>
+        <h3 style="margin-bottom: 8%">The Only Inventory You'll Ever Need !</h3>
+    </div>
+    <div class="row justify-content-center">
+        <form action="manager_home.php" method="post" class="col-sm-6">
             <div class="form-group">
-                <label class="control-label col-sm-offset-1 col-sm-2" for="user name">User Name:</label>
-                <div class="col-sm-6">          
-                    <input type="text" class="form-control" id="username" placeholder="Enter User Name" name="username" required>
-                </div>
-            </div>
+                <label class="contorl-label" for="user name">User Name:</label>
+                <input type="text" class="form-control" style="border-color: #5380b7;" id="username" placeholder="Enter User Name" name="username" required>
+            </div>              
             <div class="form-group">
-                <label class="control-label col-sm-offset-1 col-sm-2" for="password">Password:</label>
-                <div class="col-sm-6">          
-                    <input type="text" class="form-control" id="password" placeholder="Enter Password" name="password" required>
-                </div>
+                <label class="control-label" for="password">Password:</label>        
+                <input type="text" class="form-control" style="border-color: #5380b7;" id="password" placeholder="Enter Password" name="password" required>
             </div>
-            <div class="form-group">        
-                <div class="col-sm-offset-4 col-sm-8">
-                    <button type="submit" name="submit" value="Login" class="col-sm-2 btn btn-default btn-lg">Login</button>
-                    <button type="submit" name="submit" value="Register" class="col-sm-offset-2 col-sm-2 btn btn-default btn-lg">Sign Up</button>
+            <div class="row justify-content-center">        
+                <div style="padding-top: 2%">
+                    <button type="submit" name="submit" value="Login" class=" btn btn-default btn-lg" style="border-color: #5380b7; color: #5380b7;">Login</button>
                 </div>
             </div>    
         </form>
     </div>
-    <div class="footer row align-items-end">
-            
-    </div>
+    
         
    
 </div>
--- InventoryManager/manager_home.php
+++ InventoryManager/manager_home.php
@@ -25,11 +25,152 @@ if(isPostRequest()){
             var_dump($login);
         }
     }*/
-    var_dump($username);
+    /*var_dump($username);
     var_dump($password);
-    var_dump($usertype);
+    var_dump($usertype);*/
 }
 
-if($_SESSION['submit'] == 'Register'){
+/*if($_SESSION['submit'] == 'Register'){
         header('Location: register.php');
-    }
+    }*/
+?>
+
+<html lang="en">
+<head>
+  <title>Log In</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link rel="stylesheet" href="Design/design.css">
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  
+
+</head>
+<body>
+    
+<div class="container">
+    <div>
+            <div class="block1">
+                    
+            </div>
+            <div class="block2">
+                    
+            </div>
+    </div>
+    
+    <div class="row nav" style="margin-top: 2%;">
+        
+        <div class="nav-item col-sm-2" style="text-align: center; margin-top: 1%;">
+            <a href="manager_home.php" class="">Home</a>
+        </div>
+        <div class="nav-item col-sm-2" style="text-align: center; margin-top: 1%;">
+            <a href="register.php" class="">Add User</a>
+        </div>
+        <div class="form col-sm-4">
+            <form>
+                <div class="form-row">
+                    <div class="col-9">
+                        <input class="form-control" type="search" placeholder="Search Inventory" aria-label="Search">
+                    </div>
+                    <div class="col-3">
+                        <button class="btn btn-outline-success" style="border-color: #5380b7; color: #5380b7; background-color: white;" type="submit">Search</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="nav-item col-sm-2" style="text-align: center; margin-top: 1%;">
+            <a href="reports.php" class="">Reports</a>
+        </div>
+        <div class="nav-item col-sm-2" style="text-align: center; margin-top: 1%;">
+            <a href="index.php" class="">Log Out</a>
+        </div>      
+    </div>
+    
+    <div style="text-align: center; margin-top: 2%;">
+        <h2>Liquor Store Inventory</h2>
+    </div>
+    
+    <div class="row justify-content-around" style="margin-top: 2%;">
+        <div class="col-sm-offset-1 col-sm-3">
+            <div class="card card-border">
+                <div class="card-header">
+                    <h4>Best Selling</h4>
+                </div>
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item">Cras justo odio</li>
+                  <li class="list-group-item">Dapibus ac facilisis in</li>
+                  <li class="list-group-item">Vestibulum at eros</li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-sm-offset-1 col-sm-3">
+           
+            <div class="card card-border">
+                <div class="card-header">
+                    <h4>Low Inventory</h4>
+                </div>
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item">Cras justo odio</li>
+                  <li class="list-group-item">Dapibus ac facilisis in</li>
+                  <li class="list-group-item">Vestibulum at eros</li>
+                </ul>
+            </div>
+        </div>
+        <div class=" col-sm-offset-1 col-sm-3">
+            
+            <div class="card card-border">
+                <div class="card-header">
+                    <h4>Recent Sales</h4>
+                </div>
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item">Cras justo odio</li>
+                  <li class="list-group-item">Dapibus ac facilisis in</li>
+                  <li class="list-group-item">Vestibulum at eros</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    
+    <div class="row" style="margin-top: 2%;">
+        <h4>Inventory</h4>
+  
+        <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th></th>
+                        <th>Name</th>
+                        <th>Quantity</th>
+                        <th>Description</th>
+                        <th>Image</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+
+
+                <?php// foreach ($movies as $row): ?>
+                    <tr>
+                        <td><?php //echo $row['id']; ?>a</td>
+                        <td><input type="hidden" name="i-d" value="<?php //echo $row['id'] ?>" /></td>
+                        <td><?php// echo $row['MovieName']; ?>b</td>
+                        <td><?php// echo $row['ReleaseDate']; ?>c</td>
+                        <td><?php// echo $row['Description']; ?>d</td>
+                        <td><?php// echo $row['Image']; ?>e</td>
+                        <td><a class="btn" style="color:#5380b7; border-color: #5380b7;"href="edit.php?id=<?php// echo $row['id']; ?>">Edit</a></td>
+                    </tr>
+                <?php// endforeach; ?>
+                </tbody>
+            </table>
+
+            <br />
+            <!--<a href="admin_home.php">Home</a>-->
+            <a href="#">Home</a>
+        </div>
+   
+</div>
+</body>
+</html>
+
