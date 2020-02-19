adcfdfbb64879fadfc77734a491248fb23c62830
Quinn <mquinn@labs.neit.edu>
2020-02-19 16:36:36 -0500

    added getItem function to AJAX call then pushes result to SESSION

--- InventoryManager/Model/edit.php
+++ InventoryManager/Model/edit.php
@@ -1,21 +1,13 @@
 <?php
-
+include __DIR__ . '/Model/model_inventory.php';
 session_start();
 
 $id = filter_input(INPUT_POST, 'id');
-$unitPrice=filter_input(INPUT_POST, 'unitPrice');
-$purchaseAmount=filter_input(INPUT_POST, 'purchaseAmount');
 
-array_push($_SESSION['itemId'], $id);
-array_push($_SESSION['unitPrice'], $unitPrice);
-array_push($_SESSION['purchaseAmount'], $purchaseAmount);
+$answer = getItem($id);
+array_push($_SESSION['editItem'], $answer);
 
-/*
-$_SESSION["itemId"] = filter_input(INPUT_POST, 'id');
-$_SESSION["unitPrice"] = filter_input(INPUT_POST, 'unitPrice');
-$_SESSION["purchaseAmount"] = filter_input(INPUT_POST, 'purchaseAmount');
 
-*/
 
 
 ?>
