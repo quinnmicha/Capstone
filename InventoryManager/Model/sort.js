16ef82a8402f1a7618499016d982306c439cd53f
dfernandez15 <dfernandez10@email.neit.edu>
2020-02-23 23:05:55 -0500

    Sort Function Kind of Working

---
+++ InventoryManager/Model/sort.js
@@ -0,0 +1,56 @@
+function sortBy(n){
+    var table, i, x, y, count = 0;
+
+    table = document.getElementById("invTable");
+    var switching = true;
+    
+    var direction = "ascending";
+    
+    while (switching) {
+        switching = false;
+        var rows = table.rows;
+        
+        for (i = 1; i < (rows.length - 1); i++) {
+            var Switch = false;
+            
+            x = rows[i].getElementsByTagName("TD")[n];
+            y = rows[i + 1].getElementsByTagName("TD")[n];
+            
+            // Check the direction of order 
+            if (direction == "ascending") { 
+ 
+                // Check if 2 rows need to be switched 
+                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) 
+                    { 
+                        // If yes, mark Switch as needed and break loop 
+                        Switch = true; 
+                        break; 
+                    } 
+            } else if (direction == "descending") { 
+  
+                // Check direction 
+                if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) 
+                    { 
+                        // If yes, mark Switch as needed and break loop 
+                        Switch = true; 
+                        break; 
+                    } 
+            }         
+        }
+        if(Switch) {
+            rows[i].parentNode.insertBefore(rows[i+1], rows[i]);
+            switching = true;
+            
+            // Increase count for each switch 
+            count++; 
+        }else { 
+             // Run while loop again for descending order 
+                if (count == 0 && direction == "ascending") { 
+                    direction = "descending"; 
+                    switching = true; 
+                } 
+           } 
+    }
+}
+
+
