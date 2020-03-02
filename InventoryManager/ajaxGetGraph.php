9b8882dfa66b3343fbe37d480635df7572258b2f
quinnmicha <mquinn@email.neit.edu>
2020-03-01 21:15:31 -0500

    ajax call set

--- InventoryManager/ajaxGetGraph.php
+++ InventoryManager/ajaxGetGraph.php
@@ -1,9 +1,13 @@
 <?php
 include __DIR__ . '/Model/model_inventory.php';
 
-$profitData = getProfitByWeek();
-$results[0] = array(); // week 
-$results[1] = array(); // profit
+
+session_start();
+
+if($_SESSION["graphWeek"]==="YTD"){
+    $profitData = getProfitByWeek();
+    $results[0] = array(); // week 
+    $results[1] = array(); // profit
 
     foreach ($profitData as $v) {
         array_push($results[0], $v['week']);
@@ -11,5 +15,18 @@ $results[1] = array(); // profit
     }
     $jsonResults= json_encode($results);
     echo $jsonResults;
+}
+    
+else{
+    $profitData = getReportByWeek($_SESSION["graphWeek"]);
+    $results[0] = array(); // expense 
+    $results[1] = array(); // revenue
+    $results[2] = array(); // week selected
 
+    array_push($results[0], number_format($profitData['expense'], 2, '.', ''));
+    array_push($results[1], number_format($profitData['revenue'], 2, '.', ''));
+    array_push($results[2], $profitData['week']);
+    $jsonResults= json_encode($results);
+    echo $jsonResults;
+}
 ?>
